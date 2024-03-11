05a128da47dc89ab292e6995be959108c20e4b20
Folke <vossfolke@gmail.com>
2024-03-11 20:01:29 +0100

    changed hello to getGrades.

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GradeController {
 
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "HTML: hello";
+    @GetMapping("/grades")
+    public String getGrade(){
+        return "grades";
     }
+
+
 
 }
