b51552221dac444d26a65c430396d76df655e757
Folke <vossfolke@gmail.com>
2024-03-13 19:44:55 +0100

    addad data cells to the grades.html table

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -3,15 +3,33 @@ package com.ltp.gradesubmission;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class GradeController {
 
+
+    List<Grade> grades = new ArrayList<>();
     @GetMapping("/grades")
     public String getGrade(Model model){
-        Grade grade = new Grade("Harry",  "Potions", "C-");
-        model.addAttribute("grade", grade);
+        model.addAttribute("grade", grades);
         return "grades";
+    }
+
+    @GetMapping("/")
+    public String gradeForm(Model model){
+        model.addAttribute("grade", new Grade());
+        return "form";
+
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade) {
+        grades.add(grade);
+        return "redirect:/grades";
     }
 
 
